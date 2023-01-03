18a4ec2aa220094991ec5d9faa0c74a1a9ec5bbd
Khawar Hussain <57666092+khawarwaraich1995@users.noreply.github.com>
2023-01-03 14:26:13 +0500

    init admin template

---
+++ storage/framework/views/8f71a6b582cadf7481d28928cee227ca8969bbc7.php
@@ -0,0 +1,5 @@
+
+<body>
+
+<!-- <body data-layout="horizontal"> -->
+<?php /**PATH E:\laragon\www\fos\resources\views/layouts/body.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/957e4098de27ab8a1dd9e730361233a7accda7b7.php
@@ -0,0 +1,5 @@
+
+<body>
+
+<!-- <body data-layout="horizontal"> -->
+<?php /**PATH E:\laragon\www\default\resources\views/layouts/body.blade.php ENDPATH**/ ?>
