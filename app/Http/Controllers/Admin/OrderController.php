38de04d631bf92344b975b457eed9503ef212cb4
Khawar Hussain <khawarr404@gmail.com>
2023-06-30 20:30:56 +0500

    DB Class name fixed

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Order;
 use App\Models\Tenant;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\CreateOrder;
 
