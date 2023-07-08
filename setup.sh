00360f072f1155b5534916e04fc53563c8b8bca0
Khawar Hussain <khawarr404@gmail.com>
2023-07-08 12:19:35 +0500

    Extra step removed for permissions

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Copy ENV example to .env file
 cp .env.example .env
-# Give permissions to directories
-chmod -R 777 ./bootstrap/cache
-chmod -R 777 ./storage
 
 # Remove existing containers
 docker-compose down
