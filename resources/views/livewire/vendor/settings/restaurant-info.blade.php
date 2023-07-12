b736558adfe869bc233015b4a55dd652a31b14ff
Khawar Hussain <khawarr404@gmail.com>
2023-07-12 15:49:54 +0500

    Restrautn info saved with fixed

--- resources/views/livewire/vendor/settings/restaurant-info.blade.php
+++ resources/views/livewire/vendor/settings/restaurant-info.blade.php
@@ -33,9 +33,27 @@
                             </div>
                             <div class="col-lg-6 mb-2">
                                 <label class="form-label">Phone Number</label>
-                                <input type="text" class="form-control @error('phone') is-invalid @enderror"
+                                {{-- <input type="text" class="form-control @error('phone') is-invalid @enderror"
                                     wire:model.defer="phone" id="phone"
-                                    placeholder="Enter your valid phone number">
+                                    placeholder="Enter your valid phone number"> --}}
+                                <div class="input-group" data-input-flag>
+                                    <button class="btn btn-light border" type="button" data-bs-toggle="dropdown"
+                                        aria-expanded="false"><img src="{{ URL::asset('assets/images/flags/us.svg') }}"
+                                            alt="flag img" height="20" class="country-flagimg rounded"><span
+                                            class="ms-2 country-codeno">+ 1</span></button>
+                                    <input type="text"
+                                        class="form-control rounded-end flag-input @error('phone') is-invalid @enderror"
+                                        wire:model.defer="phone" placeholder="Enter number"
+                                        oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');" />
+                                    <div class="dropdown-menu w-100">
+                                        <div class="p-2 px-3 pt-1 searchlist-input">
+                                            <input type="text"
+                                                class="form-control form-control-sm border search-countryList"
+                                                placeholder="Search country name or country code..." />
+                                        </div>
+                                        <ul class="list-unstyled dropdown-menu-list mb-0"></ul>
+                                    </div>
+                                </div>
                                 @error('phone')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -103,7 +121,8 @@
                             <div class="col-lg-6 mb-2">
                                 <label class="form-label">Longitude</label>
                                 <input type="text" class="form-control @error('longitude') is-invalid @enderror"
-                                    wire:model.defer="longitude" id="longitude" placeholder="Longitude eg. 3722323.12">
+                                    wire:model.defer="longitude" id="longitude"
+                                    placeholder="Longitude eg. 3722323.12">
                                 @error('longitude')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -123,3 +142,6 @@
         </div>
     </div>
 </div>
+@push('script')
+    <script src="{{ URL::asset('assets/js/pages/flag-input.init.js') }}"></script>
+@endpush
