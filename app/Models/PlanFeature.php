2e0f4f6e476fc94dc3d33fc4b1b0643c3666ccde
AHSEN ALEE <ahsen.alee@therightsw.com>
2023-10-07 09:04:11 +0500

    Added listing functionalities

--- app/Models/PlanFeature.php
+++ app/Models/PlanFeature.php
@@ -10,4 +10,16 @@ class PlanFeature extends Model
     use HasFactory;
 
     protected $fillable = ['feature_name', 'feature_description'];
+
+    public function scopeGetList($query, $search, $sortField, $sortDirection)
+    {
+        if (!empty($search)) {
+            $query->where(function ($subQuery) use ($search) {
+                $subQuery->where('feature_name', 'like', '%' . $search . '%')
+                    ->orWhere('feature_description', 'like', '%' . $search . '%');
+            });
+        }
+
+        return $query->orderBy($sortField, $sortDirection);
+    }
 }
---
+++ resources/views/livewire/admin/plans/feature-list.blade.php
@@ -0,0 +1,111 @@
+<div class="card" id="planFeaturesList">
+    <div class="card-header border-0">
+        <div class="d-flex align-items-center">
+            <h5 class="card-title mb-0 flex-grow-1">Plan Features</h5>
+            <div class="flex-shrink-0">
+                <div class="d-flex gap-2 flex-wrap">
+                    <button class="btn btn-primary" id="remove-actions" onClick="deleteMultiple()"><i
+                            class="ri-delete-bin-2-line"></i></button>
+                    <a href="{{ route('admin.plans.features.create') }}" class="btn btn-success"><i
+                            class="ri-add-line align-bottom me-1"></i> Create Plan Features</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card-body bg-soft-light border border-dashed border-start-0 border-end-0">
+        <div class="row g-3">
+            <div class="col-xxl-4 col-sm-12">
+                <label for="search"> Search</label>
+                <div class="search-box">
+                    <input type="text" wire:model.debounce.500ms="search" id="search"
+                        class="form-control search bg-light border-light"
+                        placeholder="Search for name, description, price or something...">
+                    <i class="ri-search-line search-icon"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table align-middle mb-0">
+                <thead class="table-light">
+                    <tr>
+                        <th scope="col">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value=""
+                                    id="responsivetableCheck">
+                                <label class="form-check-label" for="responsivetableCheck"></label>
+                            </div>
+                        </th>
+                        <th class="sort text-uppercase" scope="col">Feature Name
+                        </th>
+                        <th class="sort text-uppercase" scope="col">Feature Description</th>
+                        <th class="text-uppercase" scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @isset($planFeatures)
+                        @foreach ($planFeatures as $planFeature)
+                            <tr>
+                                <th scope="row">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value=""
+                                            id="responsivetableCheck01">
+                                        <label class="form-check-label" for="responsivetableCheck01"></label>
+                                    </div>
+                                </th>
+                                <td>
+                                    {{ $planFeature?->feature_name }}
+                                </td>
+                                <td>
+                                    {{ Str::limit($planFeature->feature_description, 50, '...') }}
+                                </td>
+                                <td>
+                                    <button class="btn btn-soft-secondary btn-sm dropdown" type="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="ri-more-fill align-middle"></i>
+                                    </button>
+                                    <ul class="dropdown-menu dropdown-menu-end">
+                                        <li>
+                                            <a class="dropdown-item" href="{{ route('admin.plans.features.edit', $planFeature?->id) }}" data-id="{{ $planFeature?->id }}">
+                                                <i class="ri-edit-fill align-bottom me-2 text-muted"></i>
+                                                Edit</a>
+                                        </li>
+                                        <li><a class="dropdown-item" href="javascript:void(0);" data-id="">
+                                                <i class="ri-eye-fill align-bottom me-2 text-muted"></i>
+                                                View</a></li>
+                                        <li>
+                                            <a class="dropdown-item" wire:click="confirmDelete('{{ $planFeature?->id }}')"
+                                                href="javascript:void(0);" data-id="">
+                                                <i class="ri-delete-bin-fill align-bottom me-2 text-muted"></i>
+                                                Delete</a>
+                                        </li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endisset
+                </tbody>
+            </table>
+            {{-- <div>
+                {{ $planFeatures->links() }}
+            </div> --}}
+            @unless (count($planFeatures))
+                <div class="noresult">
+                    <div class="text-center">
+                        <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
+                            colors="primary:#121331,secondary:#08a88a" style="width:75px;height:75px">
+                        </lord-icon>
+                        <h5 class="mt-2">Sorry! No Result Found</h5>
+                        <p class="text-muted mb-0">We've searched more than 150+ plugins We
+                            did not find any
+                            plugins for you search.</p>
+                    </div>
+                </div>
+            @endunless
+
+            <!-- end table -->
+        </div>
+        <!-- end table responsive -->
+    </div>
+</div>
