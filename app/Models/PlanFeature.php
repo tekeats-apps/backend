99ea840de818fac3bd0c487bd5535a5e0e76d837
AHSEN ALEE <ahsen.alee@therightsw.com>
2023-10-07 08:17:52 +0500

    Created model and migration for db setup

---
+++ app/Models/PlanFeature.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlanFeature extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['feature_name', 'feature_description'];
+}
