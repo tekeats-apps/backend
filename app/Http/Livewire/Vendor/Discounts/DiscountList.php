b80fdd3213c40d5738ea3aecec812658bb470bee
AHSEN ALEE <ahsen.alee@therightsw.com>
2023-10-14 14:42:00 +0500

    Added delete functionality.

--- app/Http/Livewire/Vendor/Discounts/DiscountList.php
+++ app/Http/Livewire/Vendor/Discounts/DiscountList.php
@@ -12,6 +12,8 @@ class DiscountList extends Component
     public $sortField = 'created_at';
     public $sortDirection = 'desc';
 
+    protected $listeners = ['delete' => 'destroy'];
+
     public function render()
     {
         return view('livewire.vendor.discounts.discount-list', ['discounts' => $this->getDiscountsList()]);
@@ -20,5 +22,25 @@ class DiscountList extends Component
     private function getDiscountsList()
     {
         return Discount::getList($this->search, $this->sortField, $this->sortDirection)->paginate($this->perPage);
+    }
+
+    public function confirmDelete($id)
+    {
+        // Show the SweetAlert confirmation dialog
+        $this->emit('swal:confirm-delete', [
+            'title' => 'Are you sure?',
+            'text' => 'You are about to delete the discount. This action cannot be undone.',
+            'id' => $id,
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        try {
+            Discount::findOrFail($id)->delete();
+            $this->dispatchBrowserEvent('success', ['message' => 'Discount deleted successfully!']);
+        } catch (\Exception $e) {
+            session()->flash('error', 'Failed to delete discount: ' . $e->getMessage());
+        }
     }
 }
