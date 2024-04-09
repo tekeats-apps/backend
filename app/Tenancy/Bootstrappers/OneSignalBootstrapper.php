d59e02bf6299e6e57e38617eef65d844b724492b
Khawarr <khawarr404@gmail.com>
2024-04-09 12:59:11 +0500

    Boostrapper fixed for Onesignal Settings

--- app/Tenancy/Bootstrappers/OneSignalBootstrapper.php
+++ app/Tenancy/Bootstrappers/OneSignalBootstrapper.php
@@ -2,6 +2,7 @@
 
 namespace App\Tenancy\Bootstrappers;
 
+use DB;
 use App\Settings\OneSignalSettings;
 use Stancl\Tenancy\Contracts\Tenant;
 use Illuminate\Support\Facades\Config;
@@ -18,9 +19,15 @@ class OneSignalBootstrapper implements TenancyBootstrapper
 
     public function bootstrap(Tenant $tenant)
     {
+        // Check if the migration has been run
+        if (!$this->migrationHasBeenRun('2024_04_09_050400_create_onesignal_settings')) {
+            // Possibly log this situation or handle it accordingly
+            return;
+        }
+
         $apiKey = $this->oneSignalSettings->api_key;
         $appId = $this->oneSignalSettings->app_id;
-        
+
         Config::set('services.onesignal.app_id', $appId);
         Config::set('services.onesignal.rest_api_key', $apiKey);
     }
@@ -30,5 +37,10 @@ class OneSignalBootstrapper implements TenancyBootstrapper
         // You can revert to the default settings or clear the OneSignal configuration
         Config::set('services.onesignal.app_id', null);
         Config::set('services.onesignal.rest_api_key', null);
+    }
+
+    private function migrationHasBeenRun($migrationName)
+    {
+        return DB::table('migrations')->where('migration', $migrationName)->exists();
     }
 }
