96a6a3915768caf114b54b7790c06cbca7578631
Khawarr <khawarr404@gmail.com>
2024-05-13 00:40:47 +0500

    Removed extra code in settings module

--- app/Http/Controllers/API/V1/Vendor/SettingController.php
+++ app/Http/Controllers/API/V1/Vendor/SettingController.php
@@ -6,8 +6,6 @@ use Exception;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Settings\GeneralSettings;
-use App\Settings\DeliverySettings;
 use App\Http\Controllers\Controller;
 use App\Services\Platform\SettingService;
 
@@ -44,32 +42,5 @@ class SettingController extends Controller
         } catch (Exception $e) {
             return $this->exceptionResponse($e, "Failed to fetch settings.");
         }
-    }
-
-    protected function getRestaurantInfo()
-    {
-        return [
-            'name' => $this->generalSettings->name,
-            'email' => $this->generalSettings->email,
-            'phone' => $this->generalSettings->phone,
-            'address' => $this->generalSettings->address,
-            'country' => $this->generalSettings->country,
-            'city' => $this->generalSettings->city,
-            'latitude' => $this->generalSettings->latitude,
-            'longitude' => $this->generalSettings->longitude,
-        ];
-    }
-    protected function getDeliverySettings()
-    {
-        return [
-            'free_delivery' => $this->deliverySettings->free_delivery,
-            'free_delivery_charge_type' => $this->deliverySettings->free_delivery_charge_type,
-            'free_delivery_radius' => $this->deliverySettings->free_delivery_radius,
-            'delivery_charge_type' => $this->deliverySettings->delivery_charge_type,
-            'distance_unit' => $this->deliverySettings->distance_unit,
-            'distance_based_radius' => $this->deliverySettings->distance_based_radius,
-            'delivery_charges' => $this->deliverySettings->delivery_charges,
-            'range_based_charges' => $this->deliverySettings->range_based_charges,
-        ];
     }
 }
