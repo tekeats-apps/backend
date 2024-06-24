fac386fc8d3abbddb845223d3a29e6d476eb6361
Khawar Hussain <khawarr404@gmail.com>
2024-06-24 20:12:12 +0500

    Prefix admin routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -32,8 +32,9 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->group(base_path('routes/api.php'));
 
-            // Route::middleware('web')
-            //     ->group(base_path('routes/web.php'));
+            Route::middleware('web')
+                ->prefix('panel')
+                ->group(base_path('routes/web.php'));
 
             Route::middleware('api')
                 ->prefix('admin')
